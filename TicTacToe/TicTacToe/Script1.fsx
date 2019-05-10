143d658cb7ae14f4362ef189f2f2323b82e2bf8f
Tilly Davies <tillyd72@gmail.com>
2019-05-10 01:40:47 +0100

    modules added
    game split into modules

--- TicTacToe/TicTacToe/Script1.fsx
+++ TicTacToe/TicTacToe/Script1.fsx
@@ -1,53 +1,94 @@
 ﻿open System.Web.UI
 
-//GameState used to keep track of the game
-//saves which board spaces are taken 
-//should be private as its used for game logic
-type GameState = exn  // exn is a placeholder
 
-//players X and O
-type Player = PlayerX | PlayerO
+//game design
+module TicTacToeDesign =
 
-//board positions
-//board made of horizontal and vertical components
-type HorizontalPosition = Left | Center | Right 
-type VerticalPosition = Top | Center | Bottom
-type SpacePosition = HorizontalPosition * VerticalPosition
+    //players X and O
+    type Player = PlayerX | PlayerO
 
-//board spaces
-//Spaces either empty or used 
-//(used when an X or O is placed on them)
-//space made of its position and state
-type SpaceSate = Taken of Player | Empty
+    //board positions
+    //board made of horizontal and vertical components
+    type HorizontalPosition = Left | Center | Right 
+    type VerticalPosition = Top | Center | Bottom
+    type SpacePosition = HorizontalPosition * VerticalPosition
 
-type Space = {
-    position : SpacePosition 
-    state : SpaceSate
- }
+    //board spaces
+    //Spaces either empty or used 
+    //(used when an X or O is placed on them)
+    //space made of its position and state
+    type SpaceSate = Taken of Player | Empty
 
-
-//player position
-//postion player puts a counter on their turn
-type PlayerXPosition = PlayerXPosition of SpacePosition
-type PlayerOPosition = PlayerOPosition of SpacePosition
-
-//vaild places player can put a counter on their turn
-//places to put a counter decided from making a list of the positions taken
-type PlayerXValidPlaces = PlayerXPosition list
-type PlayerOValidPlaces = PlayerOPosition list
-
-//turn 
-type TurnResult = 
-    | PlayerXTurn of PlayerXValidPlaces
-    | PlayerOTurn of PlayerOValidPlaces
-    | WinGame of Player
-//When a player places their counter, this position is then taken.
-//GameState is updated to save all the taken positions by 
-//adding this position to the other taken 1s from previous turns
-//input -> output
-type PlayerXPlacesCounter = GameState * PlayerXPosition -> GameState * TurnResult
-type PlayerOPlacesCounter = GameState * PlayerOPosition -> GameState * TurnResult
-//New game made by resetting TurnResult and GameState
-type NewGame = GameState * TurnResult
+    type Space = {
+        position : SpacePosition 
+        state : SpaceSate
+    }
 
 
+    //player position
+    //postion player puts a counter on their turn
+    type PlayerXPosition = PlayerXPosition of SpacePosition
+    type PlayerOPosition = PlayerOPosition of SpacePosition
+
+    //vaild places player can put a counter on their turn
+    //places to put a counter decided from making a list of the positions taken
+    type PlayerXValidPlaces = PlayerXPosition list
+    type PlayerOValidPlaces = PlayerOPosition list
+
+    //turn 
+    type TurnResult = 
+        | PlayerXTurn of PlayerXValidPlaces
+        | PlayerOTurn of PlayerOValidPlaces
+        | WinGame of Player
+    //When a player places their counter, this position is then taken.
+    //GameState is updated to save all the taken positions by 
+    //adding this position to the other taken 1s from previous turns
+    //input -> output
+    type PlayerXPlacesCounter<'GameState> = 'GameState * PlayerXPosition -> 'GameState * TurnResult
+    type PlayerOPlacesCounter<'GameState> = 'GameState * PlayerOPosition -> 'GameState * TurnResult
+    //New game made by resetting TurnResult and GameState
+    type NewGame<'GameState> = 'GameState * TurnResult
+
+
+
+
+//game logic 
+module TicTacToeLogic =
+
+    open TicTacToeDesign
+
+    //GameState used to keep track of the game
+    //saves which board spaces are taken 
+    type GameState = {
+        spaces : Space list
+    }
+
+    //TODO: List horizontal positions 
+
+
+    //TODO: List vertial positions 
+
+
+    //TODO: Line of SpacePostions list
+
+
+    //TODO: Check for 3 in a row
+        //3 in a row vertial lines 
+        //3 in a row horizontal lines 
+        //diagonal 
+        //other diagonal 
+
+    //TODO: Check lines for win
+
+
+
+//game user interface
+module TicTacToeInterface =
+
+    open TicTacToeDesign
+
+
+
+//connect moudles and run the program
+//like a main method 
+module TicTacToeGame = 
